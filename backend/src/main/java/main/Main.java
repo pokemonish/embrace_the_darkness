6f389f34fb8df1ff57a5944f7cacb9a3ec182457
Sasha <sasha.konyshev1003@gmail.com>
2015-09-20 18:01:47 +0300

    homework

--- backend/src/main/java/main/Main.java
+++ backend/src/main/java/main/Main.java
@@ -1,36 +1,24 @@
 package main;
 
-import org.eclipse.jetty.server.Connector;
-import org.eclipse.jetty.server.Handler;
+import frontend.Frontend;
 import org.eclipse.jetty.server.Server;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
-
-import chat.WebSocketChatServlet;
 import org.eclipse.jetty.servlet.ServletHolder;
 
-
+/**
+ * @author v.chibrikov
+ */
 public class Main {
-    private static Server server;
-
     public static void main(String[] args) throws Exception {
-        server = new Server(8080);
+        Frontend frontend = new Frontend();
+
         ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+        context.addServlet(new ServletHolder(frontend), "/authform");
 
-        context.addServlet(new ServletHolder(new WebSocketChatServlet()), "/chat");
+        Server server = new Server(8080);
+        server.setHandler(context);
 
-        connectAll(server, context, "public_html");
         server.start();
-    }
-
-    private static void connectAll(final Server server, ServletContextHandler context, final String path) {
-        ResourceHandler resource_handler = new ResourceHandler();
-        resource_handler.setDirectoriesListed(true);
-        resource_handler.setResourceBase(path);
-
-        HandlerList handlers = new HandlerList();
-        handlers.setHandlers(new Handler[]{resource_handler, context});
-        server.setHandler(handlers);
+        server.join();
     }
 }
