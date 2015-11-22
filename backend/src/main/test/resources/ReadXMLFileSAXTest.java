6b20a8a3af554f44428394ec7a102cc579907256
Dmitry Tsyganov <dmitrytsgn@gmail.com>
2015-11-22 05:34:47 +0300

    correct tests

--- backend/src/main/test/resources/ReadXMLFileSAXTest.java
+++ backend/src/main/test/resources/ReadXMLFileSAXTest.java
@@ -5,6 +5,7 @@ import base.UserProfile;
 import base.WebSocketService;
 import frontend.WebSocketServiceImpl;
 import mechanics.GameMechanicsImpl;
+import mechanics.MechanicsParameters;
 import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
@@ -20,9 +21,6 @@ public class ReadXMLFileSAXTest extends Mockito {
     private static final String PASSWORD_TEST = "testPassword";
     private static final  String EMAIL_TEST = "testEmail";
 
-    private static final WebSocketService webSocketServiceMock = mock(WebSocketServiceImpl.class);
-    private static final WebSocketService webSocketService = new WebSocketServiceImpl();
-
     @Before
     public void setUp() throws Exception {
     }
@@ -32,17 +30,5 @@ public class ReadXMLFileSAXTest extends Mockito {
         UserProfile testUserFromXml = (UserProfile)ReadXMLFileSAX.readXML("data/test.xml");
         UserProfile testUser = new UserProfile(USERNAME_TEST, PASSWORD_TEST, EMAIL_TEST);
         assertEquals(testUser, testUserFromXml);
-    }
-
-    @Test
-    public void testReadXML1() throws Exception {
-        GameMechanics gameMechanicsTest =
-                (GameMechanics)ReadXMLFileSAX.readXML("data/GameMechanics.xml",
-                                                        GameMechanicsImpl.class.getName(),
-                                                        webSocketService);
-
-        assertNotNull(gameMechanicsTest);
-
-
     }
 }
