c9d62ccf1edcba9e28633afcc119038ac28c0be6
Dmitry Tsyganov <dmitrytsgn@gmail.com>
2016-01-22 16:18:02 +0300

    balance test #24

--- backend/src/main/test/messagesystem/AddressServiceTest.java
+++ backend/src/main/test/messagesystem/AddressServiceTest.java
@@ -15,21 +15,21 @@ import static org.junit.Assert.*;
  */
 public class AddressServiceTest {
 
-    private static final int MAX_SIZE = 5000;
     private static final int MIN_SIZE = 500;
-
-    Set<Address> addresses = Collections.synchronizedSet(new HashSet<>());
+    private static final int MAX_SIZE = 5000;
 
     private final class AttackThread extends Thread {
         private int times;
-        private AttackThread(int times) {
+        private Set<Address> addressSet;
+        private AttackThread(int times, Set<Address> addressSet) {
             this.times = times;
+            this.addressSet = addressSet;
         }
 
         @Override
         public void run() {
             for (int i = 0; i < times; ++i) {
-                addresses.add(addressService.getAccountServiceAddress());
+                addressSet.add(addressService.getAccountServiceAddress());
             }
         }
     }
@@ -42,24 +42,45 @@ public class AddressServiceTest {
 
     @Test
     public void testGetAccountServiceAddress() throws InterruptedException {
-        int times1 = new Random().nextInt(MAX_SIZE) + MIN_SIZE;
+        Set<Address> addresses = Collections.synchronizedSet(new HashSet<>());
+
+        startAttackThreads(addresses, addresses);
+
+        assertEquals(MAX_SIZE, addresses.size());
+    }
+
+    private void startAttackThreads(Set<Address> addresses1, Set<Address> addresses2)
+            throws InterruptedException {
+        int times1 = new Random().nextInt(MAX_SIZE - MIN_SIZE) + MIN_SIZE;
         int times2 = MAX_SIZE - times1;
 
         MessageSystem messageSystem = mock(MessageSystem.class, RETURNS_DEEP_STUBS);
         when(messageSystem.getAddressService()).thenReturn(mock(AddressService.class));
 
         for (int i = 0; i < MAX_SIZE; ++i) {
-            addressService.registerAccountService(new AccountServiceTh(messageSystem, mock(DBService.class, RETURNS_DEEP_STUBS)));
+            addressService.registerAccountService(
+                    new AccountServiceTh(messageSystem,
+                            mock(DBService.class, RETURNS_DEEP_STUBS)));
         }
 
-        Thread t1 = new AttackThread(times1);
-        Thread t2 = new AttackThread(times2);
+        Thread t1 = new AttackThread(times1, addresses1);
+        Thread t2 = new AttackThread(times2, addresses2);
         t1.start();
         t2.start();
 
         t1.join();
         t2.join();
+    }
 
-        assertEquals(MAX_SIZE, addresses.size());
+    @Test
+    public void testBalance() throws InterruptedException {
+        Set<Address> addresses1 = Collections.synchronizedSet(new HashSet<>());
+        Set<Address> addresses2 = Collections.synchronizedSet(new HashSet<>());
+
+        startAttackThreads(addresses1, addresses2);
+
+        assertFalse(addresses1.removeAll(addresses2));
+        System.out.println(addresses1.toString());
+        System.out.println(addresses2.toString());
     }
 }
